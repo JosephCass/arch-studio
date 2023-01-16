f91ea1e72a5e4edc2680199c24546650aac4abb6
JosephCass <josephcassinera@gmail.com>
2023-01-16 11:18:49 -0500

    Configured css to work on tablet size for the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import toggleIcon from "../assets/icons/icon-hamburger.svg";
 import titleIcon from "../assets/icons/Frame.svg";
+import rectangleIcon from "../assets/icons/rectangle-tablet-icon.svg";
 
 export default function Navbar() {
   let [navOpen, setNavOpen] = useState(false);
@@ -20,18 +21,30 @@ export default function Navbar() {
 
   return (
     <div className="navbar">
+      <img className="side-path-rectangle tablet" src={rectangleIcon} alt="" />
       <Link className="nav-title-link" to="/">
         <img src={titleIcon} className="navbar-title-img" />
       </Link>
+      <div className="nav-links-tablet tablet">
+        <Link className="nav-link-tablet" to="/portfolio">
+          Portfolio
+        </Link>
+        <Link className="nav-link-tablet" to="/about">
+          About Us
+        </Link>
+        <Link className="nav-link-tablet" to="/contact">
+          Contact
+        </Link>
+      </div>
 
       <img
         onClick={toggleNavbar}
-        className="navbar-toggle-icon"
+        className="navbar-toggle-icon mobile"
         src={toggleIcon}
         alt="navbar toggle icon"
       />
       {navOpen && (
-        <div className="nav-links-container" onClick={closeNavbar}>
+        <div className="nav-links-container mobile" onClick={closeNavbar}>
           <div id="navLinks" className="nav-links" ref={navLinks}>
             <Link className="nav-link" to="/portfolio">
               Portfolio
